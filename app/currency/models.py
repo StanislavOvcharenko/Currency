6b84a34cb2b61b5a39b8d1acafc93267eb14bca1
Ovcharenko Stanislav <ovcharenko.s.a.1995@gmail.com>
2022-08-01 17:26:32 +0300

    home work 6, templates

--- app/currency/models.py
+++ app/currency/models.py
@@ -7,3 +7,12 @@ class ContactUs(models.Model):
     subject = models.CharField(max_length=254)
     message = models.CharField(max_length=10_000)
     create = models.DateTimeField(auto_now_add=True)
+
+
+class Rate(models.Model):
+    base_currency_type = models.CharField(max_length=3)
+    currency_type = models.CharField(max_length=3)
+    sale = models.DecimalField(max_digits=10, decimal_places=4)
+    buy = models.DecimalField(max_digits=10, decimal_places=4)
+    source = models.CharField(max_length=64)
+    created = models.DateTimeField(auto_now_add=True)
--- app/settings/urls.py
+++ app/settings/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 
-from currency.views import contact_us_list
+from currency.views import contact_us_table, rate_table, contact_us_list
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('contactus/', contact_us_list)
+    path('contactus/table/', contact_us_table),
+    path('rate/table/', rate_table),
+    path('contactus/', contact_us_list),
 ]
