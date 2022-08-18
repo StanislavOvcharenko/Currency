d431d5dd3f0d7e726b96738e907db310a3b2dbb5
Ovcharenko Stanislav <ovcharenko.s.a.1995@gmail.com>
2022-08-18 16:03:53 +0300

    Home Work 9, Admin

--- app/settings/urls.py
+++ app/settings/urls.py
@@ -6,6 +6,7 @@ from currency import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.IndexView.as_view()),
+    path('silk/', include('silk.urls', namespace='silk')),
     path('__debug__/', include('debug_toolbar.urls')),
     path('currency/', include('currency.urls')),
 
